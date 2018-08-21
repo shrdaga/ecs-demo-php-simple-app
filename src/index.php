946e19663d931a49079737ce3f7045c08f40e350
shrdaga <42377737+shrdaga@users.noreply.github.com>
2018-08-21 20:48:12 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -16,7 +16,7 @@
         <div class="container">
             <div class="hero-unit">
                 <h1>SNET APP</h1>
-                <h2> </h2>
+                <h2> Welcome to ES</h2>
                 <p>Your application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
